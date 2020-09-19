eb67ebcca3dc67743cedef348f293f9ee32ea952
euonymus <euonymus0220@gmail.com>
2020-09-20 00:26:04 +0900

    version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as requirements_file:
 
 setuptools.setup(
     name="neo4j-bigbang",
-    version="0.0.4",
+    version="0.0.5",
     author="euonymus",
     author_email="euonymus0220@gmail.com",
     description="This is a library allows you to import csv data into neo4j database",
